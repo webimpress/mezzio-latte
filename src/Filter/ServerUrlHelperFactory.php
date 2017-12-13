50d191a0d6e57504550c93d43284f045f1f352a6
webimpress <contact@webimpress.com>
2017-12-13 10:43:10 +0000

    Added license headers and declare strict types

--- src/Filter/ServerUrlHelperFactory.php
+++ src/Filter/ServerUrlHelperFactory.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-latte for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (https://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-latte/blob/master/LICENSE.md New BSD License
+ */
+
+declare(strict_types=1);
 
 namespace Zend\Expressive\Latte\Filter;
 
